d44c35d27b520b43dc898ab4b32025ef5019d844
mihaitagonsoft <mihai.tagonsoft@gmail.com>
2024-02-07 17:37:07 +0200

    passed parameters dynamically

--- homework1_2/lib/gradient_container.dart
+++ homework1_2/lib/gradient_container.dart
@@ -1,22 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:tema1_2/styled_text.dart';
 
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer(
+    this.color1,
+    this.color2, {
+    super.key,
+  });
+  final Color color1;
+  final Color color2;
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 26, 2, 80),
-            Color.fromARGB(255, 45, 7, 98),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          colors: [color1, color2],
+          begin: startAlignment,
+          end: endAlignment,
         ),
       ),
-      child: const Center(child: StyledText()),
+      child: const Center(child: StyledText('Hello')),
     );
   }
 }
