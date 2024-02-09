87d9c851e034a33559bd22d8c5e2ecff74d3a69b
mihaitagonsoft <mihai.tagonsoft@gmail.com>
2024-02-09 09:36:37 +0200

    rendered content conditionally

--- homework2_1/lib/main.dart
+++ homework2_1/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:homework2_1/start_screen.dart';
+import 'package:homework2_1/quiz.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,20 +9,6 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 78, 13, 151),
-                Color.fromARGB(255, 107, 15, 168),
-              ],
-            ),
-          ),
-          child: const StartScreen(),
-        ),
-      ),
-    );
+    return const Quiz();
   }
 }
