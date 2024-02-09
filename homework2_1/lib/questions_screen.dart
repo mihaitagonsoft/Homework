060e42cb8bb5e228a590cc3d38a4f733ca0c9e61
mihaitagonsoft <mihai.tagonsoft@gmail.com>
2024-02-09 13:24:47 +0200

    added a shuffle function and a questions page

--- homework2_1/lib/questions_screen.dart
+++ homework2_1/lib/questions_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:homework2_1/answer_button.dart';
+import 'package:homework2_1/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -11,6 +13,30 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text('QuestionsScreen');
+    final currentQuestion = questions[0];
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30),
+            ...currentQuestion.getShuffledAnswers().map((answer) {
+              return AnswerButton(
+                answerText: answer,
+                onTap: () {},
+              );
+            })
+          ],
+        ),
+      ),
+    );
   }
 }
