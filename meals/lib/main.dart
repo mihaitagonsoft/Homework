8d1770aab526c998be58787c7708c04f48e71f3c
mihaitagonsoft <mihai.tagonsoft@gmail.com>
2024-02-22 11:04:25 +0200

    added Tab-based Navigation

--- meals/lib/main.dart
+++ meals/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:meals/screens/tabs.dart';
 
-import 'package:meals/screens/categories.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 final theme = ThemeData(
@@ -23,7 +23,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: const CategoriesScreen(),
+      home: const TabsScreen(),
     );
   }
 }
